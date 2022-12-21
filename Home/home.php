49293df534825b0f0834aee7d1020f9be65b2628
ruslan.k <ruslan.k@1fit.app>
2022-12-22 00:40:43 +0600

    main-page

--- Home/home.php
+++ Home/home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style.css"  type="text/css" />
   </head>
   <body>
     <header>
@@ -54,10 +54,188 @@
           <a id="new__label">Новые</h1>
           <a id="new__label__show-all">Показать все</a>
         </div>
-        <div class="card">
+        <div class="cards">
+
+          <div class="card__item">
+            <div class="card" 
+              style="
+                background-image: url('https://static.chocolife.me/static/upload/images/deal/for_deal_page/57000/56887/340x158/20220930089216336f9bd6f7a5.webp?1671262616'); 
+            ">
+              <div class="card__head">
+              <div class="card__tag">
+                <img src="../images/map_point.png" width="20px" height="20px"/>
+                <span>Абая-Абылайхана</span>
+              </div> 
+                <div class="card__bookmark">
+                  <img src="../images/bookmark.png" />
+                </div>
+              </div>
+            </div>
+            <div class="card__info">
+              <h4>20 января! Билеты на спектакль «Ищите женщину»</h4>
+              <h5>НРТД им. Лермонтова · 2 покупки</h5>
+              <div class="card__subinfo">
+                <div class="card__review">
+                  <img src="../images/star.png" />
+                  <h5>4.5/488 отзывов</h5>
+                </div>
+                <div class="card__price">
+                  <h3>От 5900тг</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card__item">
+            <div class="card" 
+              style="
+                background-image: url('https://static.chocolife.me/static/upload/images/deal/for_deal_page/57000/56887/340x158/20220930089216336f9bd6f7a5.webp?1671262616'); 
+            ">
+              <div class="card__head">
+              <div class="card__tag">
+                <img src="../images/map_point.png" width="20px" height="20px"/>
+                <span>Абая-Абылайхана</span>
+              </div> 
+                <div class="card__bookmark">
+                  <img src="../images/bookmark.png" />
+                </div>
+              </div>
+            </div>
+            <div class="card__info">
+              <h4>20 января! Билеты на спектакль «Ищите женщину»</h4>
+              <h5>НРТД им. Лермонтова · 2 покупки</h5>
+              <div class="card__subinfo">
+                <div class="card__review">
+                  <img src="../images/star.png" />
+                  <h5>4.5/488 отзывов</h5>
+                </div>
+                <div class="card__price">
+                  <h3>От 5900тг</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card__item">
+            <div class="card" 
+              style="
+                background-image: url('https://static.chocolife.me/static/upload/images/deal/for_deal_page/57000/56887/340x158/20220930089216336f9bd6f7a5.webp?1671262616'); 
+            ">
+              <div class="card__head">
+              <div class="card__tag">
+                <img src="../images/map_point.png" width="20px" height="20px"/>
+                <span>Абая-Абылайхана</span>
+              </div> 
+                <div class="card__bookmark">
+                  <img src="../images/bookmark.png" />
+                </div>
+              </div>
+            </div>
+            <div class="card__info">
+              <h4>20 января! Билеты на спектакль «Ищите женщину»</h4>
+              <h5>НРТД им. Лермонтова · 2 покупки</h5>
+              <div class="card__subinfo">
+                <div class="card__review">
+                  <img src="../images/star.png" />
+                  <h5>4.5/488 отзывов</h5>
+                </div>
+                <div class="card__price">
+                  <h3>От 5900тг</h3>
+                </div>
+              </div>
+            </div>
+          </div>
           
         </div>
       </div>
+
+        </div>
+      </div>
     </div>
+    <footer class="footer">
+    <div class="additional__info">
+      <div class="additional__info__header">
+        <img src="../images/addinfo.jpg"/>
+        <p>Все скидки и акции в одном месте!
+        Человеку для счастья нужно совсем мало: оставаться здоровым, иметь крепкую семью и хорошую работу, хранить гармонию души. Но иногда это не все. Не хватает какого-то приятного дополнения, например, получить бешеную скидку на популярную услугу. И этот бонус можем подарить вам именно мы! Благодаря купонам, приобретенным в нашем сервисе, вы сможете стать самым счастливым человеком в любой точке в Алматы.
+        Мы предлагаем скидки в Алматы на самые разнообразные виды деятельности и услуги, которые распределены по таким рубрикам:
+      </p>
+      </div>
+      <div class="additional__info__sub">
+        <ul id="addinfo__ul">  
+          <li>новые (самые свежие предложения);</li>
+          <li>развлечения (караоке, кинотеатры, активный отдых и досуг);</li>
+          <li>еда (рестораны, кафе, доставка и т. д.);</li>
+          <li>здоровье (врачи, анализы, медцентры, обследования);</li>
+          <li>красота (салоны красоты, а также отдельных мастеров);</li>
+          <li>спорт (услуги спортзалов, фитнес-клубов и т. д.);</li>
+          <li>туризм (туры выходного дня, гостиницы, санатории и т. д.).</li>
+          <li>услуги (авто-услуги, обучающие курсы, быт. услуги и т. д.);</li>
+          <li>товары (для школы, для дома и т. д.);</li>
+        </ul>
+        <p>
+        Покупайте купоны и экономьте на услугах и товарах!
+        B купонном сервисе Chocolife (Чоколайф) можно приобрести горящие скидки и купоны в Алматы и Казахстане с самым разным акционным диапазоном: от 30% до 90%. На сайте представлено более 500 разных возможностей для реализации личных планов. Например, вы можете в очередной раз посетить любимое кафе, но теперь со скидочным купоном на 50% оставить там намного меньшую сумму. Такой купон обойдется вам всего от 200 тенге.
+        Срок действия каждой акции в Алматы указан на нашем сайте. Эта информация поможет приобрести купон для посещения вами не только интересного предложения, но и оптимального во времени. Чтобы быстрее отыскать нужную акцию в нашем агрегаторе скидок, воспользуйтесь удобным поиском сайта онлайн по всем имеющимся рубрикам.
+        Отныне и навсегда сайт акций и скидок в Алматы должен стать вашим лучшим другом и помощником! С нашей помощью вы сэкономите уйму денежных средств, невозвратимое время и дорогое здоровье. Удобное оформление сайта, правильно подобранная цветовая гамма, удобство в расположении рубрик не оставят равнодушным ни молодежь, ни людей преклонного возраста.
+        Скидочные купоны в Алматы, представленные у нас — это гарантия настоящего качества. Мы ручаемся за каждого своего компаньона, уверены в безупречности каждого предложения, ведь все проверено лично нами! Если вы не успели применить скидку по назначению, или вас не устроило обслуживание — звоните по телефону, указанному на сайте, и наша служба заботы о пользователях обязательно даст вам все разъяснения. При возникновении вопросов или предложений также можно связаться с нами через СЗП и форму обратной связи. ❤
+        </p>
+      </div>
+    </div>
+      <div class="footer__upper">
+          <div>
+            <ul>
+              <li><h5>КОМПАНИЯ</h5></li>
+              <li>O Chocolife.me</li>
+              <li>Пресса о нас</li>
+              <li>Контакты</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li><h5>КЛИЕНТАМ</h5></li>
+              <li>Обратная связь</li>
+              <li>Вопросы и ответы</li>
+              <li>Публичная оферта</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li><h5>ПАРТНЕРАМ</h5></li>
+              <li>Для Вашего бизнеса</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li><h5>КОНТАКТЫ</h5></li>
+              <li>+7 (771) 930-02-02</li>
+              <li>+7 (727) 339-91-00</li>
+            </ul>
+            <div class="logos">
+              <img src="../images/facebook.png"/>
+              <img src="../images/instagram.png"/>
+              <img src="../images/tik-tok.png"/>
+              <img src="../images/youtube.png"/>
+            </div>
+          </div>
+          <div>
+            <ul>
+              <li><h5>НАШЕ ПРИЛОЖЕНИЕ</h5></li>
+              <li>Chocolife.me теперь еще удобнее и всегда под рукой!</li>
+            </ul>
+            <div class="appstores">
+              <div class="appstores__upper">
+                <img src="../images/googleplay.png"/>
+                <img src="../images/appstore.png" width="153px" height="45px"/>
+              </div>
+              <img src="../images/appgallery.png"  width="153px" height="45px"/>
+            </div>
+          </div>
+      </div>
+      <div class="footer__down">
+          <h5>Chocolife.me 2011-2022 · Карта сайта</h5>
+          <img src="../images/depositphoto.svg" height="30px"/>
+      </div>
+    </footer>
   </body>
 </html>
